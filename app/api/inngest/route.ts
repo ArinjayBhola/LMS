af5cad3947f911e7d282ec3cd57dda20a4a11b9a
ArinjayBhola <arinjay26bhola@gmail.com>
2025-12-22 14:01:49 +0530

    vercel err

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,7 +2,7 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { createNewUser, generateCourseOutline, generateStudyTypeContent, helloWorld } from "@/inngest/function";
 
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
